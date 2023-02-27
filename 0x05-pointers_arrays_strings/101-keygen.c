b182e79ccfc68d1f381dbed7bcc1c8dcc54069d9
waribokosilas <waribokosilas2@gmail.com>
2023-02-27 04:08:12 -0800

    DGT

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,46 +1,35 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * _atoi -  converts a string to an integer
- * @s: string to be converted
+ * main - program that generates random valid 
+ * passwards for the program 101-crackme
  *
- * Return: the int converted from the string
+ * Return: Always 0 (Success)
  */
-int _atoi(char *s)
+int main(void)
 {
-	int i, d, n, len, f, digit;
+	int pass[100];
+	int i, sum, n;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
+	sum = 0;
 
-	while (s[len] != '\0')
-		len++;
+	srand(time(NULL));
 
-	while (i < len && f  == 0)
+	for (i = 0; i < 100; i++)
 	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
 		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
 		}
-		i++;
 	}
 
-	if (f == 0)
-		return (0);
-
-	return (n);
+	return (0);
 }
