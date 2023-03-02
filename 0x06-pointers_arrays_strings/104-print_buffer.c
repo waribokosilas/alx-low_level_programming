eb2dbd2d89ca3d0d4de96cb0e16c7b3d577d2088
waribokosilas <waribokosilas2@gmail.com>
2023-03-02 05:23:37 -0800

    DGT

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -25,5 +25,26 @@ void print_buffer(char *b, int size)
 		printf("%08x: ", o);
 		for (i = 0; i < 10; i++)
 		{
+			if (i < j)
+				printf("%02x", *(b + o + i));
+			else
+				printf("  ");
+			if (i % 2)
+			{
+				printf(" ");
+			}
+		}
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + o + i);
 
-
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		o += 10;
+	}
+}
