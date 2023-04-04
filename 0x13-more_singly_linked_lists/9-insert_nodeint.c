dd7375a356c4f00c177065e85effe6b7790053f6
waribokosilas <waribokosilas2@gmail.com>
2023-04-04 04:29:31 -0700

    do great things

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -38,4 +38,48 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (new);
 		}
 		else
+			temp = temp->next;
+	}
+
+	return (NULL);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
